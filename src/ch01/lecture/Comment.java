bd48ae7588732ef3fb1110e9dafb7dbe2d8aadb3
WonsikJung13 <110441649+WonsikJung13@users.noreply.github.com>
2022-08-17 12:13:33 +0900

    first commit

---
+++ src/ch01/lecture/Comment.java
@@ -0,0 +1,20 @@
+package ch01.lecture;
+
+public class Comment {
+	public static void main(String[] args) {
+		// 주석 : 프로그램 실행에 영향을 미치지 않음
+		// 한줄 주석
+		
+		/*
+		 여러줄 주석
+		 여러줄!
+		*/
+		
+		// 단축키 : ctrl + /
+		
+		System.out.println("Hello World!");
+		System.out.println("자바는 쉽지 않다");
+		System.out.println("잘 배워서 써먹어 보자!");
+		System.out.println("이건 단축키로 만든거");
+	}
+}
